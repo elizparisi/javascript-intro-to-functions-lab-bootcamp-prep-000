88d93e921f291aeabfa09fadc16686c0af785e20
Elizabeth Parisi <parisielizabeth@gmail.com>
2018-08-15 18:32:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,9 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string)
+function sayHiToGrandma(string)
+  if { (string.toLowerCase() === string)
   return "I can\'t' hear you";
-} 
-else if 
+} else if { (string.toUpperCase() === string)
+  return "YES INDEED!";
+} else if { (string.mixedCase)
