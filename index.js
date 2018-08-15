547b2f6dae67b70f433216009700686715e5b2e4
Elizabeth Parisi <parisielizabeth@gmail.com>
2018-08-15 18:33:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,4 +19,4 @@ function sayHiToGrandma(string)
   return "I can\'t' hear you";
 } else if { (string.toUpperCase() === string)
   return "YES INDEED!";
-} else if { (string.mixedCase)
+} else if { (string === "I love you grandma")
